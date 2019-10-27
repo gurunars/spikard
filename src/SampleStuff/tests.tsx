662942fd5d4628a8707460d7131ddac50c779dee
Anton Berezin <gurunars@gmail.com>
2019-10-27 22:19:06 +0200

    ff

--- src/SampleStuff/tests.tsx
+++ src/SampleStuff/tests.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 import SampleStuff from ".";
 
@@ -9,5 +10,15 @@ describe("SampleStuff", () => {
       .create(<SampleStuff title="FooBar" onClick={() => {}} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should process clicks correctly", () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(<SampleStuff title="FooBar" onClick={onClick} />);
+    wrapper
+      .find("button")
+      .at(0)
+      .simulate("click");
+    expect(onClick).toHaveBeenCalled();
   });
 });
