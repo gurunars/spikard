a9cccbe9b66d3b6ca11c3cf53c4a98a529e30db5
Anton Berezin <gurunars@gmail.com>
2019-10-27 12:20:47 +0200

    ff

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,11 @@
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve("ts-loader")
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]],
+    },
   });
-  //config.plugins.push(new TSDocgenPlugin()); // optional
-  config.resolve.extensions.push(".ts", ".tsx");
+  config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
