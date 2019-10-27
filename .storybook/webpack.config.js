73b3c7c40c6c833aa17c980f0785ce041140a538
Anton Berezin <gurunars@gmail.com>
2019-10-27 12:08:13 +0200

    ff

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,9 @@
-const path = require("path");
-
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    include: path.resolve(__dirname, "../src"),
-    loader: require.resolve("awesome-typescript-loader"),
-    options: {
-      configFileName: "tsconfig.json",
-      presets: [["react-app", { flow: false, typescript: true }]]
-    }
+    loader: require.resolve("ts-loader")
   });
+  //config.plugins.push(new TSDocgenPlugin()); // optional
   config.resolve.extensions.push(".ts", ".tsx");
   return config;
 };
