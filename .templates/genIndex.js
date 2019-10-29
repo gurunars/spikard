f60d8628832e60bcec0177300095ab197cea4520
Anton Berezin <anton.berezin@nordea.com>
2019-10-29 15:01:49 +0200

    ff

---
+++ .templates/genIndex.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+function main() {
+  const files = fs.readdirSync("src");
+  const lines = files
+    .filter(it => fs.existsSync("src/" + it + "/index.ts") || fs.existsSync("src/" + it + "/index.tsx"))
+    .map(it => "export { default as " + it + " } from \"./" + it + "\";")
+    .join("\n");
+  //fs.writeFile()
+}
+
+main();
