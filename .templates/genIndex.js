6515fdc88045f1fe39845df1f1d209f474cd99e4
Anton Berezin <anton.berezin@nordea.com>
2019-10-30 11:28:25 +0200

    Added stuff to package.json

--- .templates/genIndex.js
+++ .templates/genIndex.js
@@ -4,7 +4,7 @@ function main() {
   const files = fs.readdirSync("src");
   const lines = files
     .filter(it => fs.existsSync("src/" + it + "/index.ts") || fs.existsSync("src/" + it + "/index.tsx"))
-    .map(it => "export { default as " + it + " } from \"./" + it + "\";")
+    .map(it => "export { default as " + it + " } from \"../src/" + it + "\";")
     .join("\n");
   if (!fs.existsSync("generated")) {
     fs.mkdirSync("generated")
