c54676ac287f11f4a7870c428b4204b601f8e14c
Anton Berezin <gurunars@gmail.com>
2019-11-01 18:47:15 +0200

    ff

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,38 @@
 module.exports = {
-    mode: "production",
+  mode: "production",
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
-    resolve: {
-        extensions: [".ts", ".tsx"]
-    },
+  resolve: {
+    extensions: [".ts", ".tsx"]
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                loader: require.resolve('babel-loader'),
-                options: {
-                    presets: [['react-app', { flow: false, typescript: true }]],
-                },
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
-            }
-        ]
-    },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [["react-app", { flow: false, typescript: true }]]
+        }
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      }
+    ]
+  },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    }
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM"
+  }
 };
