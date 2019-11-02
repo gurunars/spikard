eeabc99a6a8aacf1f8523e616c9e34f7c12c874c
Anton Berezin <gurunars@gmail.com>
2019-11-02 13:23:06 +0200

    ff

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,27 +1,17 @@
+const custom = require("../webpack.config.js");
+
 module.exports = ({ config }) => {
   const rules = config.module.rules;
 
-  rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve('babel-loader'),
-    options: {
-      presets: [['react-app', { flow: false, typescript: true }]],
-    },
-  });
-
   // Disable regular svg handling
-  const rule = rules.find(rule => rule.test.test('.svg'));
+  const rule = rules.find(rule => rule.test.test(".svg"));
   if (rule != null) {
     const rs = rule.test.toString().replace("svg|", "");
     rule.test = RegExp(rs.substr(1, rs.length - 2));
   }
 
-  // Add custom svg handling
-  rules.push({
-    test: /\.svg$/,
-    use: ['@svgr/webpack'],
-  })
+  custom.module.rules.forEach(it => rules.push(it));
+  custom.resolve.extensions.forEach(it => config.resolve.extensions.push(it));
 
-  config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
