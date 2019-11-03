cc835e5316ce878b25645a20e39e8cc875bf524a
Anton Berezin <gurunars@gmail.com>
2019-11-04 01:44:21 +0200

    Added svg icon

--- src/SampleStuff/index.tsx
+++ src/SampleStuff/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 
+import Icon from "./icon.svg";
+
 const SampleStuff = (props: {
   title: string,
   onClick: () => void
 }): React.ReactElement<any> => (
-    <button
-      style={{
-        cursor: "pointer"
-      }}
-      onClick={props.onClick}
-    >
-      {props.title}
-    </button>
+    <div>
+
+      <button
+        style={{
+          cursor: "pointer",
+          alignItems: "center",
+          display: "flex",
+          padding: "5px",
+          color: "green"
+        }}
+        onClick={props.onClick}
+      >
+        <Icon color="green" />
+        {props.title}
+      </button>
+    </div>
   );
 
 export default SampleStuff;
