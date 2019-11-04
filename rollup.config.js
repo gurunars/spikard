e2f5a7c749151f3a2a2505572e18870353fa2f1b
Anton Berezin <gurunars@gmail.com>
2019-11-04 02:08:52 +0200

    ff

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import typescript from "rollup-plugin-typescript";
+import svgr from "@svgr/rollup";
 
 export default {
-  entry: "./src/index.tsx",
-  dest: "./dist/bundle.js",
-  format: "iife",
-  plugins: [typescript()]
+  input: "generated/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "esm",
+    sourcemap: true
+  },
+  plugins: [typescript(), svgr()]
 };
