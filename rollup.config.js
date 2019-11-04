f25b1b026f01ea075d6e2a389c910718a8878366
Anton Berezin <gurunars@gmail.com>
2019-11-04 02:26:14 +0200

    Added hooks

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+
 import typescript from "rollup-plugin-typescript";
 import svgr from "@svgr/rollup";
+
+function generateIndex() {
+  const files = fs.readdirSync("src");
+  const lines = files
+    .filter(
+      it =>
+        fs.existsSync("src/" + it + "/index.ts") ||
+        fs.existsSync("src/" + it + "/index.tsx")
+    )
+    .map(it => "export { default as " + it + ' } from "../src/' + it + '";')
+    .join("\n");
+  if (!fs.existsSync("generated")) {
+    fs.mkdirSync("generated");
+  }
+  fs.writeFileSync("generated/index.ts", lines);
+}
 
 export default {
   input: "generated/index.ts",
@@ -8,5 +26,13 @@ export default {
     format: "esm",
     sourcemap: true
   },
-  plugins: [typescript(), svgr()]
+  plugins: [
+    {
+      buildStart() {
+        generateIndex();
+      }
+    },
+    typescript(),
+    svgr()
+  ]
 };
