43a39ce94ce0d31d350115bb86f5f0131932be4c
Anton Berezin <gurunars@gmail.com>
2019-11-05 18:21:21 +0200

    ff

--- src/SampleStuff/index.tsx
+++ src/SampleStuff/index.tsx
@@ -6,21 +6,21 @@ const SampleStuff = (props: {
   title: string;
   onClick: () => void;
 }): React.ReactElement<any> => (
-  <div>
-    <button
-      style={{
-        cursor: "pointer",
-        alignItems: "center",
-        display: "flex",
-        padding: "5px",
-        color: "green"
-      }}
-      onClick={props.onClick}
-    >
-      <Icon color="green" />
-      {props.title}
-    </button>
-  </div>
-);
+    <div>
+      <button
+        style={{
+          cursor: "pointer",
+          alignItems: "center",
+          display: "flex",
+          padding: "5px",
+          color: "green"
+        }}
+        onClick={props.onClick}
+      >
+        <Icon color="green" />
+        {props.title}
+      </button>
+    </div>
+  );
 
 export default SampleStuff;
