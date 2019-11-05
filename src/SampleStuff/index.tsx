1ce8b8dc27e82656893216f94b08739afb859d41
Anton Berezin <gurunars@gmail.com>
2019-11-05 12:36:37 +0200

    ff

--- src/SampleStuff/index.tsx
+++ src/SampleStuff/index.tsx
@@ -3,25 +3,24 @@ import React from "react";
 import Icon from "./icon.svg";
 
 const SampleStuff = (props: {
-  title: string,
-  onClick: () => void
+  title: string;
+  onClick: () => void;
 }): React.ReactElement<any> => (
-    <div>
-
-      <button
-        style={{
-          cursor: "pointer",
-          alignItems: "center",
-          display: "flex",
-          padding: "5px",
-          color: "green"
-        }}
-        onClick={props.onClick}
-      >
-        <Icon color="green" />
-        {props.title}
-      </button>
-    </div>
-  );
+  <div>
+    <button
+      style={{
+        cursor: "pointer",
+        alignItems: "center",
+        display: "flex",
+        padding: "5px",
+        color: "green"
+      }}
+      onClick={props.onClick}
+    >
+      <Icon color="green" />
+      {props.title}
+    </button>
+  </div>
+);
 
 export default SampleStuff;
