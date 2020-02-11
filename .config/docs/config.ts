6ec8fa37deab91861271025a08d403167d6ee290
Anton Berezin <gurunars@gmail.com>
2020-02-11 22:34:23 +0000

    ff

--- .config/docs/config.ts
+++ .config/docs/config.ts
@@ -1,5 +1,5 @@
 import { configure } from "@storybook/react"
 
-const req = require.context("../../src", true, /story\.tsx$/)
+const req = require.context("../../packages", true, /story\.tsx$/)
 
 configure(() => req.keys().forEach(req), module)
